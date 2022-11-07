fa290b1e2ece43ad11dbf5396cf2ee6feb839262
고유라 <koyura183982@naver.com>
2022-11-07 11:25:20 +0900

    Revert ""fix: global exception handler sout 추가""
    This reverts commit 279f48b7db39bfdece19ae17f34e0b9ec4114409

--- backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalControllerAdvice.java
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalControllerAdvice.java
@@ -12,7 +12,6 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity exception(Exception e) {
         String message = new Gson().toJson(e.getMessage());
-        System.out.println("error message: " + message);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
     }
 }
